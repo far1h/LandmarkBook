a15d29369f3a0d00635f0f7d1407657bcdaf6d2d
far1h <farihmuhammad45@gmail.com>
2024-08-25 22:55:33 +0700

    Connecting data to tableView

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -11,6 +11,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet weak var tableView: UITableView!
     
+    var landmarkNames : [String] = ["Amalienborg Palace", "LEGOLAND", "Christiansborg Palace", "Little Mermaid", "Frederik Church", "Rosenborg Castle", "Frederiksborg Castle", "Round Tower", "Kronborg Castle", "Tivoli Gardens"]
+    
+    var landmarkImages : [UIImage] = [
+        UIImage(named: "Amalienborg_Palace")!,
+        UIImage(named: "LEGOLAND")!,
+        UIImage(named: "Christiansborg_Palace")!,
+        UIImage(named: "Little_Mermaid")!,
+        UIImage(named: "Frederik_Church")!,
+        UIImage(named: "Rosenborg_Castle")!,
+        UIImage(named: "Frederiksborg_Castle")!,
+        UIImage(named: "Round_Tower")!,
+        UIImage(named: "Kronborg_Castle")!,
+        UIImage(named: "Tivoli_Gardens")!,
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,37 +33,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         
-        var landmarkNames : [String] = [
-            "Amalienborg_Palace",
-            "LEGOLAND",
-            "Christiansborg_Palace",
-            "Little_Mermaid",
-            "Frederik_Church",
-            "Rosenborg_Castle",
-            "Frederiksborg_Castle",
-            "Round_Tower",
-            "Kronborg_Castle",
-            "Tivoli_Gardens"
-        ]
-        
-        var landmarkImages : [UIImage] = [
-            UIImage(named: "Amalienborg_Palace")!,
-            UIImage(named: "LEGOLAND")!,
-            UIImage(named: "Christiansborg_Palace")!,
-            UIImage(named: "Little_Mermaid")!,
-            UIImage(named: "Frederik_Church")!,
-            UIImage(named: "Rosenborg_Castle")!,
-            UIImage(named: "Frederiksborg_Castle")!,
-            UIImage(named: "Round_Tower")!,
-            UIImage(named: "Kronborg_Castle")!,
-            UIImage(named: "Tivoli_Gardens")!,
-        ]
 
 
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return landmarkNames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -57,7 +47,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         var content = cell.defaultContentConfiguration()
 //        content.text = "TableView"
 //        content.secondaryText = "Test"
-        content.text = "test"
+        content.text = landmarkNames[indexPath.row]
         cell.contentConfiguration = content
         return cell
     }
